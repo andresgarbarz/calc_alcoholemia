32bde8d89a8e32eac85380594ac8c47fc6c03cd9
Andrés Garbarz <andresgarbarz@gmail.com>
2022-11-14 19:12:44 -0300

    Alcoholemia calculus update
    ..

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ form.addEventListener(onsubmit, (event) => { event.preventDefault(); calc(event)
 
 const calc = (event) => {
     event.preventDefault();
-    console.log(document.querySelector('input[name="cuerpo"]:checked').id);
-    const myFormData = new FormData(event.target);
-    const formDataObj = Object.fromEntries(myFormData.entries());
-    console.log(formDataObj);
+    const data = event.target.elements;
     ml = 50;
     ga = 40;
-    masaEtanol = ml * (ga/100) *0.789;
-    console.log(masaEtanol);
+    masaEtanol = data.ml.value * (data.ga.value/100) * 0.789;
+    nums = {hombre: {astenico: 0.85, atletico: 0.76, picnico: 0.64}, mujer: {astenico: 0.76, atletico: 0.67, picnico: 0.58}};
+    const alcoholemia = masaEtanol / (data.peso.value * nums[data.sexo.value][data.cuerpo.value]);
+    console.log(alcoholemia)
 }
